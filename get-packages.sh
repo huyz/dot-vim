7c75241fafce6fb9850af2f1345638d2a8bccf43
huyz <h-git@huyz.us>
2011-08-07 14:33:39 +1000

    Added coffee-script

--- get-packages.sh
+++ get-packages.sh
@@ -198,6 +198,7 @@ fi
 [ -d nerdcommenter ] || git clone http://github.com/scrooloose/nerdcommenter.git
 [ -d snipmate.vim ] || git clone http://github.com/msanders/snipmate.vim.git
 [ -d vim-gnupg ] || git clone git://gitorious.org/vim-gnupg/vim-gnupg.git
+[ -d vim-coffee-script ] || git clone git://github.com/kchmck/vim-coffee-script.git
 
 # vim-solarized is contained within the larger repository solarized, so check
 # first for the global location
